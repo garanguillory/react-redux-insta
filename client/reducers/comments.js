a8b5e1ef39ea852214e83e8bc76d3e21f820e27b
Garan Guillory <garanguillory@gmail.com>
2016-09-05 20:28:20 -0500

    setting up instagram imitation

---
+++ client/reducers/comments.js
@@ -0,0 +1,8 @@
+
+
+function comments(state = [], action){
+	console.log(`state: ${state} and action: ${action}`);
+	return state;
+}
+
+export default comments;
---
+++ client/reducers/posts.js
@@ -0,0 +1,8 @@
+
+
+function posts(state = [], action){
+	console.log(`state: ${state} and action: ${action}`);
+	return state;
+}
+
+export default posts;
